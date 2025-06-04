a0304f37ee6129dcf1aa0dae342fb8f7f03ccab5
capt-farvez <farvez165@gmail.com>
2025-06-05 02:24:58 +0600

    Add diabetes prediction model implementation with data preparation and prediction functions

---
+++ utils/diabetes_predictor.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+# Load and clean diabetes dataset
+def load_and_prepare_data():
+    df = pd.read_csv('data/diabetes.csv')
+    df = df.rename(columns={'DiabetesPedigreeFunction': 'DPF'})
+    df_copy = df.copy(deep=True)
+    df_copy[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']] = df_copy[
+        ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']
+    ].replace(0, np.nan)
+
+    df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
+    df_copy['BloodPressure'].fillna(df_copy['BloodPressure'].mean(), inplace=True)
+    df_copy['SkinThickness'].fillna(df_copy['SkinThickness'].median(), inplace=True)
+    df_copy['Insulin'].fillna(df_copy['Insulin'].median(), inplace=True)
+    df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
+
+    return df_copy
+
+# Train and save model
+def train_and_save_model():
+    df = load_and_prepare_data()
+    X = df.drop(columns='Outcome')
+    y = df['Outcome']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+    classifier = RandomForestClassifier(n_estimators=20)
+    classifier.fit(X_train, y_train)
+
+    with open('models/diabetes-prediction-rfc-model.pkl', 'wb') as file:
+        pickle.dump(classifier, file)
+    print("Training Successful ✅")
+
+# Predict using saved model
+def predict_diabetes(input_data):
+    with open('models/diabetes-prediction-rfc-model.pkl', 'rb') as file:
+        classifier = pickle.load(file)
+
+    feature_names = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
+                     'Insulin', 'BMI', 'DPF', 'Age']
+
+    input_df = pd.DataFrame([input_data], columns=feature_names)
+    prediction = classifier.predict(input_df)
+    return prediction[0]
