ffc00c4c592ac40ccd4a096d0c70285267e8535e
capt-farvez <farvez.capt@gmail.com>
2025-06-05 12:23:12 +0600

    Refactor app.py to remove debug mode and configure port from environment variable , ready for hosting.

--- app.py
+++ app.py
@@ -33,5 +33,9 @@ def predict():
     except Exception as e:
         return render_template('diabetes.html', prediction_text=f"Input Error: {e}")
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
